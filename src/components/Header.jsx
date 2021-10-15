6cc1ff4dd197166c48f99ebafcf39fc3e2072e35
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-10-15 21:40:18 +0200

    REFACTORING. Use nomenclature BEM into 'page-header' component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import CreateIcon from "@mui/icons-material/Create";
 
 function Header() {
 	return (
-		<header>
-			<h1>
-				<CreateIcon />
+		<header class="page-header">
+			<h1 class="page-header__title">
+				<CreateIcon class="page-header__logo" />
 				Notes
 			</h1>
 		</header>
