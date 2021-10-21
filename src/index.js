5e6ee55b5ceb5d284569464cde780ea3f019e545
simmiggrwl <simmiggrwl@gmail.com>
2021-10-21 23:59:21 +0530

    Feat: error and success messages on completion or failure of a request

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const options = {
+    position: positions.BOTTOM_RIGHT,
+    timeout: 3000,
+    offset: '30px',
+  }
+
+const Root = () => (
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+  )
+
+ReactDOM.render(<Root />, document.getElementById("root"));
